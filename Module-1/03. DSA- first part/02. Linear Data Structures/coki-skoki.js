f60c4730dedaa398272cec037510165b67b8c0fd
Milena Atanasova <atamasowa40@gmail.com>
2018-02-11 12:23:41 +0200

    Added optimization.

--- Module-1/03. DSA- first part/02. Linear Data Structures/coki-skoki.js
+++ Module-1/03. DSA- first part/02. Linear Data Structures/coki-skoki.js
@@ -5,28 +5,24 @@ const gets = returnedObj.gets;
 const print = returnedObj.print;
 
 const solve = (() => {
-    const jump = (currIndx) => {
-        let el = buildingsTokens[currIndx];
-        let count = 0;
-
-        for (let i = currIndx; i < buildingsTokens.length; i++) {
-            if (el < buildingsTokens[i + 1]) {
-                count++;
-                el = buildingsTokens[i + 1];
+    const jump = (currIndex) => {
+        for (let j = currIndex + 1; j < buildingsTokens.length; j++) {
+            if (buildingsTokens[currIndex] < buildingsTokens[j]) {
+                jumps[currIndex] = jumps[j] + 1;
+                return;
             }
         }
-
-        return count;
+        return;
     };
 
     const n = +gets();
     const buildingsTokens = gets().split(' ').map(Number);
 
-    const jumps = [];
+    const jumps = Array.from({ length: n }, (x) => 0);
 
-    buildingsTokens.forEach((token, inx) => {
-        jumps.push(jump(inx));
-    });
+    for (let i = buildingsTokens.length - 2; i >= 0; i--) {
+        jump(i);
+    }
 
     print(Math.max(...jumps));
     print(jumps.join(' '));
