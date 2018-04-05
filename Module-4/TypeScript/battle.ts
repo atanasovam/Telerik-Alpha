cc8757cbc2d648207c29d03702e1a2ac4ed94d80
Milena Atanasova <atamasowa40@gmail.com>
2018-04-06 01:34:25 +0300

    bug fix

--- Module-4/TypeScript/battle.ts
+++ Module-4/TypeScript/battle.ts
@@ -1,4 +1,4 @@
-import { gets, print } from './gets.print';
+// import { gets, print } from './gets.print';
 
 class Battle {
     static _counter: number;
@@ -15,6 +15,10 @@ class Battle {
     constructor(row, col) {
         this._row = row;
         this._col = col;
+    }
+
+    public print(player1, player2?): void {
+        console.log(player1 + `:${player2}`);
     }
 
     get counter() {
@@ -55,7 +59,7 @@ class Battle {
 
         this._result = this.currentOutput(value);
 
-        print(this._result);
+        this.print(this._result);
 
         if (this._result === 'Booom') {
             Battle._p1 += 1;
@@ -108,16 +112,32 @@ class Battle {
     }
 }
 
-const size: Array<number> = gets().split(' ').map(Number);
+const gets: Array<string> = (function (): Array<string> {
+    return [
+        '9 10',
+        '0 98 3 520 367 394 452 8 460 65',
+        '282 42 162 110 316 451 20 21 529 156',
+        '472 265 513 516 120 444 428 299 82 218',
+        '161 221 528 257 373 34 4 202 329 433',
+        '83 253 558 483 499 60 374 424 43 475',
+        '460 450 3 387 68 173 324 299 547 93',
+        '531 66 119 31 171 63 103 169 274 154',
+        '355 505 93 187 120 128 136 335 177 334',
+        '345 16 246 466 373 120 259 298 389 260'
+    ];
+})();
+
+let i: number = 0;
+const size: Array<number> = gets[i].split(' ').map(Number);
 
 const battle: Battle = new Battle(size[0], size[1]);
-let command: string;
+let command: Array<string>;
 
 while (true) {
-    command = gets().split(' ');
+    command = gets[i].split(' ');
 
     if (command[0] === 'END') {
-        print(battle.calculateResult(battle.boat1) + ':' + battle.calculateResult(battle.boat2));
+        battle.print(battle.calculateResult(battle.boat1), battle.calculateResult(battle.boat2));
         break;
     }
 
